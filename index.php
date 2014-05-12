30c03ac834f877a82f7b42d966d8c77357fe769f
Mohammad Alipour <info@malipour.ir>
2014-05-12 23:25:37 +0330

    Design and Create Login Page
    Start Project and create login page on index.php file

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<?php 
-echo 'welcome to clinicians';
-echo "1393/2/15";
+<?php
+ if(isset($_GET['exit'])){
+	user_logout();
+	header('Location: index.php?page=home');
+	exit();
+	 
+}
 ?>
+<!DOCTYPE html>
+<html>
+<?php  include("header.php"); ?>
+<body>
+<center>
+<br>
+<br>
+
+
+	<div class="main_login">
+	<div style="margin-top: 60px;margin-right: 50px;">
+		<form action='login.php' method='post'>
+			<div align='right' class='l-box'>
+				 <table width='360' height='75' border='0'>
+		  			<tr>
+		    			<td width='37' valign='middle'>&nbsp;</td>
+		    			<td width='144' valign='middle'><input name='username' type='text' class='input' /></td>
+		    			<td width='70' valign='middle'>نام کاربری</td>
+		   			</tr>
+		  			<tr>
+		    		<td valign='middle'><input class="btn2"  type='submit' name='submit' id='login' value='ورود' /></td>
+		    		<td valign='middle'><input name='password' type='password'  class='input'/></td>
+		    		<td valign='middle'>کلمه عبور</td>
+		   			</tr>
+				</table>
+		<div style='color:#F00;'>".$_SESSION['user_error']."</div>
+			</div>
+		</form>
+	
+	</div>
+	</div>
+	
+</center>
+</body>
+</html>
