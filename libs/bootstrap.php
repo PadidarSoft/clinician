bdc9d1361fd4ffd6489932e0f9e35f0b875db69a
Mohammad Alipour <alipour@padidarsoft.ir>
2014-07-18 23:20:10 +0430

    Remove Project

--- libs/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include('class.db.php');
-include('func.php');
-include('class.date.php');
-$database = new db("root", "", "localhost", "clinician", array(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf-8'"));
-?>
